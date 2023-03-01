6fbf0acd83525a481f0cf4cb990e045624c7e742
Zuhaib Amjad <umar0423@gmail.com>
2023-03-01 16:44:10 +0500

    Add rooms detail structure component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App">Cherry Rooms</div>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './pages/Sidebar';
+import Rooms from './components/rooms/Rooms';
+import RoomDetails from './components/rooms/RoomDetails';
 
+const App = () => (
+  <>
+    <Sidebar />
+    <Routes>
+      <Route path="/" element={<Rooms />} />
+      <Route path="/rooms/:id" element={<RoomDetails />} />
+    </Routes>
+  </>
+);
 export default App;
