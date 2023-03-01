d7dd57080db1c02c2722bf02e35a3a198053d7d8
Joel-100 <joelkugonza@gmail.com>
2023-03-01 15:15:35 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Sidebar from './pages/sideBar';
 import Rooms from './components/rooms/Rooms';
 import RoomDetails from './components/rooms/RoomDetails';
 
-
 const App = () => (
-  <> 
-     <Sidebar />
+  <>
+    <Sidebar />
     <Routes>
       <Route path="/" element={<Rooms />} />
       <Route path="/rooms/:id" element={<RoomDetails />} />
