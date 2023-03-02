31c4404c1476349da76f4cc9306c75e8521df62a
adelguit <adeelguitoun@gmail.com>
2023-03-02 07:35:55 +0100

    Update redux user actions

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -3,7 +3,9 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 const SIGNUP_URL = 'http://localhost:3000/signup';
 const LOGIN_URL = 'http://localhost:3000/login';
 const LOGOUT_URL = 'http://localhost:3000/logout';
-const USER_ACTION = 'user_action';
+const USER_SIGNED = 'user_signup';
+const USER_LOGGED = 'user_login';
+const USER_LOGGEDOUT = 'user_loggedout';
 
 // User login and signup helper function
 const userAuth = async (url, userInfo) => {
@@ -21,19 +23,19 @@ const userAuth = async (url, userInfo) => {
 };
 
 // User SignUp thunk
-export const userSignUp = createAsyncThunk(USER_ACTION, async (userInfo) => {
+export const userSignUp = createAsyncThunk(USER_SIGNED, async (userInfo) => {
   const data = await userAuth(SIGNUP_URL, userInfo);
   return data;
 });
 
 // User Login thunk
-export const userLogin = createAsyncThunk(USER_ACTION, async (userInfo) => {
+export const userLogin = createAsyncThunk(USER_LOGGED, async (userInfo) => {
   const data = await userAuth(LOGIN_URL, userInfo);
   return data;
 });
 
 // User Logout thunk
-export const userLogout = createAsyncThunk(USER_ACTION, async () => {
+export const userLogout = createAsyncThunk(USER_LOGGEDOUT, async () => {
   const res = fetch(LOGOUT_URL, {
     method: 'delete',
     headers: {
