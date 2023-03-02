19e85fe0ddaf8773d3f4765ab3d12c7c883327ec
adelguit <adeelguitoun@gmail.com>
2023-03-02 17:28:54 +0100

    Fix eslint errors

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
-const Splash = () => {
-  return (
-    <>
-      <h1>Splash Screen</h1>;
-      <div>
-        <Link to="/login">Sign In</Link>;<Link to="/signup">Sign Up</Link>;
-      </div>
-    </>
-  );
-};
+
+const Splash = () => (
+  <>
+    <h1>Splash Screen</h1>
+    ;
+    <div>
+      <Link to="/login">Sign In</Link>
+      ;
+      <Link to="/signup">Sign Up</Link>
+      ;
+    </div>
+  </>
+);
 
 export default Splash;
