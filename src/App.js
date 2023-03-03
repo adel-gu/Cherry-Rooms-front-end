c140daa8d9c576082fc613bc20af8c9d0c0533ce
Aminu Rabiu <arabiu033@gmail.com>
2023-03-03 21:17:33 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Sidebar from './pages/Sidebar';
 // import Rooms from './components/rooms/Rooms';
 import RoomDetails from './components/rooms/RoomDetails';
 import ReservationCard from './components/reservations/ReservationCard';
-import MyReservation from './components/reservations/MyReservation';
+// import MyReservation from './components/reservations/MyReservation';
 
 const App = () => (
   <>
     <Sidebar />
     <Routes>
-      <Route path="/" element={<MyReservation />} />
+      <Route path="/" element={<ReservationCard />} />
       <Route path="/reservations/:id" element={<ReservationCard />} />
       <Route path="/rooms/:id" element={<RoomDetails />} />
     </Routes>
