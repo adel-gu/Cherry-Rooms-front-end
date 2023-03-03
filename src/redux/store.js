2aa1da71d9ce8ef3f9808598548d883a3a8754a7
Aminu Rabiu <arabiu033@gmail.com>
2023-03-03 16:31:26 +0100

    Update the store with the reservation reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import roomsReducer from './rooms/roomsSlice';
+import reservationReducer from './reservations/reservationSlice';
 
 const store = configureStore({
   reducer: {
     rooms: roomsReducer,
+    reservations: reservationReducer,
   },
 });
 export default store;
