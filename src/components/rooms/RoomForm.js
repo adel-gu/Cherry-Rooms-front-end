2c1ab7fadd6ae59844ad734b624b04d0794e3d43
adel <adeelguitoun@gmail.com>
2023-03-06 10:39:52 +0100

    Style the room form page

--- src/components/rooms/RoomForm.js
+++ src/components/rooms/RoomForm.js
@@ -1,77 +1,77 @@
 const RoomForm = () => {
   return (
     <div className="main-container">
-      <div className="text-center">
-        <h1 className="text-4xl font-medium text-lime-400">Create a Room</h1>
-        <p className="max-w-xs pt-6">
-          Login and start your rooms reservations all over the world
-        </p>
+      <div className="pt-24">
+        <div className="text-center">
+          <h1 className="text-4xl font-medium text-lime-400">Create a Room</h1>
+          <p className="pt-6">Add Room information...</p>
+        </div>
+        <form className="md:w-1/2 px-10 md:px-0 mt-5 mx-auto">
+          <ul>
+            <li>
+              <input
+                type="text"
+                name="name"
+                placeholder="Room Title"
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:outline-2 focus:border-transparent"
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                name="image"
+                placeholder="Room Image"
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:outline-2 focus:border-transparent"
+              />
+            </li>
+            <li>
+              <input
+                type="number"
+                name="beds"
+                placeholder="Number of beds"
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
+              />
+            </li>
+            <li>
+              <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                min="0.00"
+                step="0.01"
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                name="city"
+                placeholder="City"
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
+              />
+            </li>
+            <li>
+              <textarea
+                name="description"
+                placeholder="Room description..."
+                required
+                className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
+              ></textarea>
+            </li>
+            <button
+              type="submit"
+              className="w-full bg-lime-500 text-white w-1/2 py-3 mx-auto mt-2 block hover:bg-lime-600"
+            >
+              Submit
+            </button>
+          </ul>
+        </form>
       </div>
-      <form>
-        <ul>
-          <li>
-            <input
-              type="text"
-              name="name"
-              placeholder="Room Title"
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:outline-2 focus:border-transparent"
-            />
-          </li>
-          <li>
-            <input
-              type="text"
-              name="image"
-              placeholder="Room Image"
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:outline-2 focus:border-transparent"
-            />
-          </li>
-          <li>
-            <input
-              type="number"
-              name="beds"
-              placeholder="Number of beds"
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
-            />
-          </li>
-          <li>
-            <input
-              type="number"
-              name="price"
-              placeholder="Price"
-              min="0.00"
-              step="0.01"
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
-            />
-          </li>
-          <li>
-            <input
-              type="text"
-              name="city"
-              placeholder="City"
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
-            />
-          </li>
-          <li>
-            <textarea
-              name="description"
-              placeholder="Room description..."
-              required
-              className="w-full my-3 focus:ring-lime-600 focus:border-transparent"
-            ></textarea>
-          </li>
-          <button
-            type="submit"
-            className="w-full bg-lime-500 text-white w-1/2 py-3 mx-auto mt-2 block hover:bg-lime-600"
-          >
-            Create new room
-          </button>
-        </ul>
-      </form>
     </div>
   );
 };
