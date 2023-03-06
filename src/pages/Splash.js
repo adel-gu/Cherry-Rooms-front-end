c53d8ecfa3ceb7c9dfc3c45bd91d2bcb43e0d0ad
Zuhaib Amjad <umar0423@gmail.com>
2023-03-06 15:21:52 +0500

    Update links in sidebar

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,16 +1,13 @@
 import { Link } from 'react-router-dom';
 
 const Splash = () => (
-  <>
+  <div className="main-container">
     <h1>Splash Screen</h1>
-    ;
     <div>
       <Link to="/login">Sign In</Link>
-      ;
       <Link to="/signup">Sign Up</Link>
-      ;
     </div>
-  </>
+  </div>
 );
 
 export default Splash;
