f83ad78f480b318fd3a494c8553984a9d0a5925e
adel <adeelguitoun@gmail.com>
2023-03-07 22:09:11 +0100

    Implement user logout link

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrentUser, userLogout } from '../redux/users/usersSlice';
 import { FaSwatchbook } from 'react-icons/fa';
 import {
   AiFillDatabase,
@@ -7,15 +9,21 @@ import {
   AiFillBank,
   AiOutlineLogin,
 } from 'react-icons/ai';
+import { BiLogOutCircle } from 'react-icons/bi';
 import { HiMenu } from 'react-icons/hi';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { RiLoginCircleFill, RiCloseLine } from 'react-icons/ri';
 import '../styles/sidebar.css';
 
 const Sidebar = () => {
   const [navHide, setNavHide] = useState(false);
-
   const displayHideNavbar = () => setNavHide(!navHide);
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(userLogout());
+  };
 
   return (
     <>
@@ -99,22 +107,14 @@ const Sidebar = () => {
 
         <div className="px-4 flex flex-col space-y-1">
           <NavLink
-            to="#"
+            to="/"
             className="flex items-center cursor-pointer gap-2 rounded-lg hover:bg-prime hover:text-white px-4 py-2 text-gray-500"
+            onClick={handleClick}
           >
             <span>
-              <AiOutlineLogin />
+              <BiLogOutCircle />
             </span>
-            <span>Sign up</span>
-          </NavLink>
-          <NavLink
-            to="#"
-            className="flex items-center cursor-pointer gap-2 rounded-lg hover:bg-prime hover:text-white px-4 py-2 text-gray-500"
-          >
-            <span>
-              <RiLoginCircleFill />
-            </span>
-            <span>Log In</span>
+            <span>Logout</span>
           </NavLink>
         </div>
 
