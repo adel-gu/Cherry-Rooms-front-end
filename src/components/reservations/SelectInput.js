0d73019203a2acae25bb6400b9dea07b60c33930
arabiu033 <arabiu033@gmail.com>
2023-03-07 08:21:31 +0100

    Update the SelectionInput to use rooms state

--- src/components/reservations/SelectInput.js
+++ src/components/reservations/SelectInput.js
@@ -1,40 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Select from 'react-select';
 import './css/select.css';
 
-const roomProp = [
-  {
-    id: 1,
-    name: 'Luxury 1',
-    beds: 2,
-    price: 10.99,
-    description: 'Its a wonderful suite',
-    image: 'https://images.unsplash.com/photo-1580237072617-771c3ecc4a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN1aXRlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    city: 'Islamabad',
-  },
-  {
-    id: 2,
-    name: 'Luxury 2',
-    beds: 2,
-    price: 10.99,
-    description: 'Its a wonderful suite',
-    image: 'https://images.unsplash.com/photo-1580237072617-771c3ecc4a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN1aXRlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    city: 'Islamabad',
-  },
-  {
-    id: 3,
-    name: 'Luxury 3',
-    beds: 2,
-    price: 10.99,
-    description: 'Its a wonderful suite',
-    image: 'https://images.unsplash.com/photo-1580237072617-771c3ecc4a24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN1aXRlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
-    city: 'Islamabad',
-  },
-];
-
 export default function SelectInput({ onChange, label }) {
-  const roomOptions = roomProp.map((room) => ({ value: room.id, label: room.name }));
+  const { rooms } = useSelector((state) => ({ ...state.rooms }));
+  const roomOptions = rooms.map((room) => ({ value: room.id, label: room.name }));
 
   return (
     <div>
