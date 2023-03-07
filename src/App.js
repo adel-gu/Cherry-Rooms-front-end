6e684ec9128d7b279ab441c515d68b33cc34ec7d
arabiu033 <arabiu033@gmail.com>
2023-03-07 08:22:15 +0100

    Update the necessary path for the reservations and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './pages/Sidebar';
-import Rooms from './components/rooms/Rooms';
-import RoomDetails from './components/rooms/RoomDetails';
 import ReservationCard from './components/reservations/ReservationCard';
 import MyReservation from './components/reservations/MyReservation';
+import Rooms from './components/rooms/Rooms';
+import RoomDetails from './components/rooms/RoomDetails';
 import Splash from './pages/Splash';
 import SignUp from './components/user/Signup';
 import Login from './components/user/Login';
@@ -12,12 +12,14 @@ const App = () => (
   <>
     <Sidebar />
     <Routes>
-      <Route path="/" element={<MyReservation/>} />
-      <Route path="/reservations/:id" element={<ReservationCard/>} />
+      <Route path="/" element={<Splash />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/rooms" element={<Rooms />} />
       <Route path="/rooms/:id" element={<RoomDetails />} />
+      <Route path="/reservations" element={<MyReservation />} />
+      <Route path="/reservation" element={<ReservationCard />} />
+      <Route path="/rooms/:id/reservation" element={<ReservationCard />} />
     </Routes>
   </>
 );
