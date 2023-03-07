35e2f652846f932b58c36f4877b532fe59c65470
arabiu033 <arabiu033@gmail.com>
2023-03-07 09:34:16 +0100

    Update the roomdetails to pass the test

--- src/components/reservations/RoomDetails.js
+++ src/components/reservations/RoomDetails.js
@@ -5,8 +5,8 @@ import style from './css/RoomDetails.module.css';
 
 export default function RoomDetails({ room }) {
   return (
-    <ul className={style.container} style={{ backgroundImage: `url(${room.image})`, backgroundColor: 'rgba(18, 23, 37, 0.6)' }}>
-      { Object.entries(room).map(([k, v]) => (k !== 'image' && k !== 'id' ? <Field key={k} k={k} value={v} /> : null)) }
+    <ul className={style.container} style={{ backgroundImage: `url(${room?.image})`, backgroundColor: 'rgba(18, 23, 37, 0.6)' }}>
+      { Object.entries(room || []).map(([k, v]) => (k !== 'image' && k !== 'id' ? <Field key={k} k={k} value={v} /> : null)) }
     </ul>
   );
 }
